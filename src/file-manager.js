9b06377ead6f1c4646de58bf0a6d3416e28386fb
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-02-04 21:18:33 +0100

    feat: implement readline, farewell and cur dir messages, LS and quit commands

--- src/file-manager.js
+++ src/file-manager.js
@@ -1,9 +1,88 @@
 import fs from 'node:fs/promises';
+import path from 'node:path';
+import { stdin, stdout } from 'node:process';
+import { createInterface } from 'node:readline';
 
-const username = process.env.npm_config_username?.replace('_', ' ') ?? 'Guest';
+const username = process.env.npm_config_username?.replace('_', ' ') ?? 'User';
+const initialDir = path.resolve(process.env.home);
+
+let currentDir = initialDir;
 
 const printWelcomeMessage = () => {
   console.log(`Welcome to the File Manager, ${username}!`);
 };
 
+const printFarewellMessage = () => {
+  console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+};
+
+const printCurrentDir = () => {
+  console.log(`You are currently in ${currentDir}`);
+};
+
+const readline = createInterface({
+  input: stdin,
+  output: stdout,
+});
+
+const fileSystem = {
+  ls: async () => {
+    try {
+      const list = await fs.readdir(currentDir);
+
+      const isFileDir = async (dir, file) =>
+        (await fs.stat(path.join(dir, file))).isDirectory();
+
+      const formatList = await Promise.all(
+        list.map(async (file) => {
+          const fileType = (await isFileDir(currentDir, file))
+            ? 'Directory'
+            : 'File';
+          const formatFile = { name: file, type: fileType };
+
+          return formatFile;
+        })
+      );
+
+      const sortedList = formatList
+        .sort((a, b) => {
+          if (a.type === b.type) {
+            return a.name.localeCompare(b.name);
+          }
+          return a.type === 'Directory' ? -1 : 1;
+        })
+        .map((file, index) => {
+          file.index = index;
+
+          return file;
+        });
+
+      console.log(sortedList);
+    } catch (err) {
+      console.error(err.message);
+    }
+  },
+};
+
+readline.on('line', (data) => {
+  switch (data) {
+    case 'quit':
+      readline.close();
+      break;
+
+    case 'ls':
+      fileSystem.ls();
+      break;
+
+    default:
+      console.log('Invalid input');
+      break;
+  }
+});
+
+readline.on('close', () => {
+  printFarewellMessage();
+});
+
 printWelcomeMessage();
+printCurrentDir();
